0fe8d4501ea1bb47bc2f758dd7cee5a843e866b7
yanlizhao <yanlizhao@fb.com>
2020-03-16 16:15:39 -0700

    fix tests that could have racy script module instantiation
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/34792
    
    it is not thread safe to initiate script module in multiple threads.
    
    for both test_remote_script_module and test_torchscript_functions_not_supported, it is possible that client thread is initiating MyScriptModule while server thread is initiating it as well in the same rank process.
    
    removing MyScriptModule instatiation in client thread, it is not needed actually.
    ghstack-source-id: 100266609
    
    Differential Revision: [D20463234](https://our.internmc.facebook.com/intern/diff/D20463234/)

--- torch/testing/_internal/distributed/rpc/jit/rpc_test.py
+++ torch/testing/_internal/distributed/rpc/jit/rpc_test.py
@@ -2,6 +2,7 @@ import unittest
 from typing import Dict, Tuple
 
 import torch
+import torch.distributed as dist
 import torch.distributed.rpc as rpc
 from torch import Tensor
 from torch.testing._internal.dist_utils import dist_init, worker_name
@@ -566,6 +567,14 @@ class JitRpcTest(LocalRRefTest, JitRpcAsyncOpTest, RpcAgentTestFixture):
     def test_torchscript_functions_not_supported(self):
         dst_worker_name = worker_name((self.rank + 1) % self.world_size)
 
+        my_local_script_module = MyScriptModule(self.rank)
+
+        # It is not thread safe to instantiate MyScriptModule in multiple threads,
+        # wait for local MyScriptModule instantiation to finish,
+        # otherwise it could instantiate MyScriptModule in parallel with
+        # server thread in the below
+        dist.barrier()
+
         # rpc_sync still accepts script class and run it in
         # the same code path as python call.
         ret = rpc.rpc_sync(
@@ -586,7 +595,7 @@ class JitRpcTest(LocalRRefTest, JitRpcAsyncOpTest, RpcAgentTestFixture):
             TypeError, "pickle"
         ):
             ret = rpc.rpc_async(
-                dst_worker_name, MyScriptModule(self.rank).forward, args=()
+                dst_worker_name, my_local_script_module.forward, args=()
             )
 
     @dist_init
@@ -626,7 +635,7 @@ class JitRpcTest(LocalRRefTest, JitRpcAsyncOpTest, RpcAgentTestFixture):
 
         api._ignore_rref_leak = True
 
-        local_ret = MyScriptModule(self.rank).forward() + torch.ones(self.rank)
+        local_ret = torch.ones(self.rank) + torch.ones(self.rank)
 
         n = self.rank + 1
         dst_rank = n % self.world_size
